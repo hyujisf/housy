59e67f5dfaf951be4ca03b5cb9f9a440696e7153
hyujisf <canqalloid.me@gmail.com>
2023-03-04 17:55:12 +0700

    change api url

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const API = axios.create({
   // baseURL: "https://3408-2404-8000-1004-b94f-71a6-be6-bec4-1ca1.ap.ngrok.io/api/v1/",
   // baseURL: process.env.REACT_APP_API,
-  baseURL: "https://housy-api.up.railway.app/api/v1",
+  baseURL: "https://api-housy.up.railway.app/api/v1",
 
 
 });
