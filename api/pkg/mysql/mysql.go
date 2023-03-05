5667a0c8247a879e1cf0f3606b610b75befe07c7
hyujisf <canqalloid.me@gmail.com>
2023-03-05 10:01:59 +0700

    change data to mysql

--- api/pkg/mysql/mysql.go
+++ api/pkg/mysql/mysql.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -18,20 +18,17 @@ func DatabaseInit() {
 	// }
 
 	// DB_TYPE := os.Getenv("DB_TYPE")
-	DB_HOST := os.Getenv("DB_HOST")
+	// DB_HOST := os.Getenv("DB_HOST")
 	DB_USER := os.Getenv("DB_USER")
 	DB_PASSWORD := os.Getenv("DB_PASSWORD")
 	DB_NAME := os.Getenv("DB_NAME")
 	DB_PORT := os.Getenv("DB_PORT")
 
 	var err error
-	// if DB_TYPE == "mysql" {
-	// 	dsn := DB_USER + ":" + DB_PASSWORD + "@tcp(127.0.0.1:" + DB_PORT + ")/" + DB_NAME + "?charset=utf8mb4&parseTime=True&loc=Local"
-	// 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	// } else {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	// }
+	dsn := DB_USER + ":" + DB_PASSWORD + "@tcp(127.0.0.1:" + DB_PORT + ")/" + DB_NAME + "?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
